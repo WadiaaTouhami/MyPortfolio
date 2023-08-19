e0eeb5d5b67b1311d5bdabc277e20afeccb6c481
WadiaaTouhami <wadiahtouhemi@gmail.com>
2023-08-19 10:07:58 +0100

    modify the behavior of navbar when clicking on the link

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const hamburger = document.querySelector('.hamburger')
-hamburger.onclick = function() {
-    navBar = document.querySelector('.nav-bar')
-    navBar.classList.toggle('active')
-}
+const hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+  navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+};
+const navLinks = document.querySelectorAll(".nav-bar li");
+console.log(navLinks);
+navLinks.forEach((element) => {
+  element.onclick = function () {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+  };
+});
